84a6f3891d0dff440c4bd78be649306275665436
Daan van Ham <daan@vanham.io>
2015-05-05 10:52:56 +0200

    test/api/issue.js * updated test for it should always expect json results

--- test/api/issue.js
+++ test/api/issue.js
@@ -18,9 +18,10 @@ lab.experiment('API', function() {
 	lab.experiment('GET ' + base, function() {
 		lab.test('should return a list', function(done) {
 			server.inject({method: 'GET', url: base}, function(response) {
+				var result = response.result.result;
+
 				Code.expect(response.statusCode).to.equal(200);
-				Code.expect(response.result).to.be.an.array();
-				Code.expect(response.headers['content-type']).to.include('application/json');
+				Code.expect(result).to.be.an.array();
 
 				done();
 			});
@@ -30,10 +31,11 @@ lab.experiment('API', function() {
 	lab.experiment('POST ' + base, function() {
 		lab.test('should return a model', function(done) {
 			server.inject({method: 'POST', url: base, payload: data}, function(response) {
+				var result = response.result.result;
+
 				Code.expect(response.statusCode).to.equal(201);
-				Code.expect(response.result).to.be.an.object();
-				Code.expect(response.result.title).to.equal(data.title);
-				Code.expect(response.headers['content-type']).to.include('application/json');
+				Code.expect(result).to.be.an.object();
+				Code.expect(result.title).to.equal(data.title);
 
 				server.inject({method: 'DELETE', url: base + '/' + response.result.id}, function(response){
 					done();
@@ -45,10 +47,11 @@ lab.experiment('API', function() {
 	lab.experiment('PUT' + base + '/issue/' + data.id, function() {
 		lab.test('should save changes and return a model', function(done) {
 			server.inject({method: 'PUT', url: base + '/' + data.id, payload: newData}, function(response) {
+				var result = response.result.result;
+
 				Code.expect(response.statusCode).to.equal(200);
-				Code.expect(response.result).to.be.an.object();
-				Code.expect(response.result.title).to.equal(newData.title);
-				Code.expect(response.headers['content-type']).to.include('application/json');
+				Code.expect(result).to.be.an.object();
+				Code.expect(result.title).to.equal(newData.title);
 
 				done();
 			});
@@ -56,10 +59,11 @@ lab.experiment('API', function() {
 
 		lab.test('can create if not exists', function(done) {
 			server.inject({method: 'PUT', url: base + '/z1237fghijk'}, function(response) {
+				var result = response.result.result;
+
 				Code.expect(response.statusCode).to.equal(200);
-				Code.expect(response.result).to.be.an.object();
-				Code.expect(response.result.id).to.equal('z1237fghijk');
-				Code.expect(response.headers['content-type']).to.include('application/json');
+				Code.expect(result).to.be.an.object();
+				Code.expect(result.id).to.equal('z1237fghijk');
 
 				server.inject({method: 'DELETE', url: base + '/z1237fghijk'}, function(response){
 					done();
@@ -72,14 +76,17 @@ lab.experiment('API', function() {
 		lab.test('should delete model', function(done) {
 			server.inject({method: 'DELETE', url: base + '/' + data.id}, function(response) {
 				Code.expect(response.statusCode).to.equal(204);
-				Code.expect(response.headers['content-type']).to.include('application/json');
+				Code.expect(response.result).to.equal(null);
 
-				server.inject({method: 'GET', url: base + '/' + data.id}, function(response) {
-					Code.expect(response.statusCode).to.equal(404);
-					Code.expect(response.headers['content-type']).to.include('application/json');
+				done();
+			});
+		});
 
-					done();
-				});
+		lab.test('model should be gone', function(done) {
+			server.inject({method: 'GET', url: base + '/' + data.id}, function(response) {
+				Code.expect(response.statusCode).to.equal(404);
+
+				done();
 			});
 		});
 	});
