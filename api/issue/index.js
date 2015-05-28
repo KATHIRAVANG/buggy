18fabd24fe0c2c8d0f2450c8179e89fef6ce4bb0
Daan van Ham <daan@vanham.io>
2015-05-28 19:45:19 +0200

    api/issue/index.js + use Joi for basic route validation

--- api/issue/index.js
+++ api/issue/index.js
@@ -8,31 +8,75 @@ exports.register = function(server, options, next) {
 	server.route({
 		method: 'GET',
 		path: base,
-		handler: api.list
+		handler: api.list,
+		config: {
+			validate: {
+				query: {}
+			}
+		}
 	});
 
 	server.route({
 		method: 'POST',
 		path: base,
-		handler: api.create
+		handler: api.create,
+		config: {
+			validate: {
+				payload: {
+					title: Joi.string().min(3).required(),
+					description: Joi.string().min(10).required(),
+					state: Joi.string().valid('open', 'closed')
+				},
+				query: {}
+			}
+		}
 	});
 
 	server.route({
 		method: 'GET',
 		path: base + '/{id}',
-		handler: api.fetch
+		handler: api.fetch,
+		config: {
+			validate: {
+				params: {
+					id: Joi.string().alphanum().length(11)
+				}
+			}
+		}
 	});
 
 	server.route({
 		method: 'PUT',
 		path: base + '/{id}',
-		handler: api.upsert
+		handler: api.upsert,
+		config: {
+			validate: {
+				params: {
+					id: Joi.string().alphanum().length(11)
+				},
+				payload: {
+					title: Joi.string().min(3),
+					description: Joi.string().min(10),
+					state: Joi.string().valid('open', 'closed')
+				},
+				query: {}
+			}
+		}
 	});
 
 	server.route({
 		method: 'DELETE',
 		path: base + '/{id}',
-		handler: api.remove
+		handler: api.remove,
+		config: {
+			validate: {
+				params: {
+					id: Joi.string().alphanum().length(11)
+				},
+				payload: {},
+				query: {}
+			}
+		}
 	});
 
 	next();
