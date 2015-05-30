3a4fd4681aa7d31ff8be91af1de1a9427b4ad137
Daan van Ham <daan@vanham.io>
2015-05-30 21:51:14 +0200

    Gulp + added task for concatination css/js + added task for html minification * updated gulpfile with Wanted module * script and style now also write the not-minified files to the build folder

--- gulp/task/concat-css.js
+++ gulp/task/concat-css.js
@@ -3,6 +3,7 @@
 module.exports = function(stream, devour) {
 	return stream
 		.pipe(devour.plugin('myth'))
+		.pipe(devour.plugin('concat', 'bundle.css'))
 		.pipe(devour.write('./css'))
 		.pipe(devour.plugin('minify-css'))
 		.pipe(devour.plugin('rename', devour.min))
--- gulp/task/concat-js.js
+++ gulp/task/concat-js.js
@@ -1,13 +1,10 @@
 'use strict';
 
-var sourcemaps = require('gulp-sourcemaps');
-
 module.exports = function(stream, devour) {
 	return stream
-		.pipe(sourcemaps.init())
+		.pipe(devour.plugin('concat', 'bundle.js'))
 		.pipe(devour.write('./js'))
 		.pipe(devour.plugin('uglify'))
 		.pipe(devour.plugin('rename', devour.min))
-		.pipe(sourcemaps.write('./'))
 		.pipe(devour.write('./js'));
 };
